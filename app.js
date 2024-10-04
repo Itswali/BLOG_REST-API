1abe9557c1cf2f177b357e286f30d736ace2d06a
Itswali <luckywali0986@gmail.com>
2024-10-04 14:21:55 +0500

    introduced User signup route in the applicatoin

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ dotenv.config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const connectMongodb = require("./init/mongodb");
-
+const { authRoute } = require("./routes")
 
 const app = express();
 
@@ -12,6 +12,10 @@ connectMongodb();
 
 app.use(express.json({limit: "500mb"}));
 app.use(bodyParser.urlencoded({limit: "500mb", extended: true}));
+
+//route section
+
+app.use("/api/v1/auth", authRoute)
 
 
 module.exports = app;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
 const User = require("./User");
 
-modules.exports = { User };
+module.exports = { User };
