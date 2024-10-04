b165e57e1f137db15742299e4da9dc2a47603561
Itswali <luckywali0986@gmail.com>
2024-10-04 14:01:20 +0500

    introdduced User model

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {type: String, required: true},
+  email: {tyoe: String, required: true, unique: true, trim: true},
+  password: {type: String, required: true, minlength: 6},
+  //role: 1 -> super admin, role: 2 -> normal admin, role: 3 normal user
+  role: { type: Number, default: 3}
+}, {timestamps: true})
+
+const User = mongoose.model("user", userSchema);
+
+modules.exports = User;
